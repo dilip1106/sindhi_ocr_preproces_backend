e3056a31f0d22b915096372be63d8b3fc93ce5d0
Dilipkumar Teli <dilipkumar.teli@vectorconsulting.in>
2026-04-14 21:18:21 +0530

    fixed the ocr

--- ocr.py
+++ ocr.py
@@ -4,8 +4,8 @@ import os
 import numpy as np
 import re
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
 
 LANG   = 'snd'
 CONFIG = '--oem 3 --psm 3'
